8a6e7ac7fa2114b8fb8b4d10e4a82798bbec4168
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-03-01 14:21:22 +0100

    balance endpoint done

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors = require('cors');
 import * as rootController from './controller/root.controller';
+import * as balanceController from './controller/balance.controller';
 
 const app = express();
 
 app.use(cors());
 app.set('port', process.env.PORT || 3000);
 app.get('/', rootController.index);
+app.get('/balance', balanceController.index);
 
 export default app;
---
+++ server/src/controller/balance.controller.ts
@@ -0,0 +1,35 @@
+import {Request, Response} from 'express'
+import { isEmpty, isLength, startsWith } from "lodash";
+import Web3 from 'web3';
+import { infuraMain } from '../constants';
+
+const web3 = new Web3(infuraMain.href);
+
+export const index = (req: Request, res: Response) => {
+    console.log(`Balance Path for Address: ${req.header("address")}`);
+    
+    const address: string | undefined = req.header("address");
+    
+    if(address !== undefined && isValidAddress(address)) {
+      web3.eth.getBalance(address.trim())
+        .then((balance: string) => {
+          res.json({
+            balance: balance
+          })
+        }
+      ).catch(
+        (err: any) => console.log(`Error in web3: ${err.message}`)
+      )
+    } else {
+      res.json({
+          message: 'ERROR: Address is not valid'
+      });
+    }
+}
+
+const isValidAddress = (address: string) => {
+  return !isEmpty(address)
+    && isLength(address.length)
+    && address.length === 42
+    && startsWith(address, '0x');
+}
